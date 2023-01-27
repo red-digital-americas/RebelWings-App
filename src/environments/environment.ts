24d15c371b8d276da4a99c5ab799bd94b3aafe0a
16Daniel <102830697+16Daniel@users.noreply.github.com>
2023-01-27 09:39:54 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiURL: 'http://operamx.mooo.com/back/api_rebel_wings/api/',
-  apiURL: "https://localhost:44334/api/"
+  apiURL: 'http://operamx.mooo.com/back/api_rebel_wings/api/',
+  //apiURL: "https://localhost:44334/api/"
 };
 
 /*
